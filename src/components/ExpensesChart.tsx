c42f660190ee50465e0f1468c5ebe681919162e0
gitmurali <murali.nutalapatii@gmail.com>
2022-04-25 23:18:12 +0100

    feature: expenses chart

--- src/components/ExpensesChart.tsx
+++ src/components/ExpensesChart.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
-import { Paper } from "@mui/material";
+import { Grid, Paper, Typography, Box } from "@mui/material";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -10,47 +10,60 @@ type Props = {
 };
 
 export default function ExpensesChart({ data }: Props) {
-    const options = {
-      plugins: {
-        legend: {
-          display: true,
-        },
+  const options = {
+    plugins: {
+      maintainAspectRatio: false,
+      legend: {
+        display: false,
       },
-    };
-    const pieData = {
-      labels: data.map((item: any) => item.category.name),
-      datasets: [
-        {
-          label: "expenses",
-          options: {
-            responsive: true,
-          },
-          data: data.map((item: any) => item.price),
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
+    },
+  };
+  const pieData = {
+    labels: data.map((item: any) => item.category.name),
+    datasets: [
+      {
+        label: "expenses",
+        options: {
+          responsive: true,
         },
-      ],
-    };
-    return (
-      <Paper elevation={3}>
-        <div style={{ position: "relative", height: "500px", width: "500px" }}>
-          <Pie data={pieData} options={options} />
-        </div>
-      </Paper>
-    );
+        data: data.map((item: any) => item.price),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <Paper elevation={3} sx={{ mt: 3, width: 350 }}>
+      <Box
+        sx={{
+          height: 350,
+          p: 3,
+        }}
+      >
+        <Grid container direction="column">
+          <Grid item xs={6}>
+            <Typography variant="h5">Total Expenses</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Pie data={pieData} options={options} style={{ marginTop: 10 }} />
+          </Grid>
+        </Grid>
+      </Box>
+    </Paper>
+  );
 }
