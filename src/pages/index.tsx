65b0111e83693b0c7dcb5a410e3b1e90e2476470
gitmurali <murali.nutalapatii@gmail.com>
2022-05-07 11:01:15 +0100

    chore: remove ssr on default page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,32 @@
-import { GetServerSideProps } from "next";
-import { Session } from "next-auth/core/types";
-import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+import { useSession } from "next-auth/react";
 import Dashboard from "../components/dashboard";
 import { fetchExpenses, fetchUser } from "../utils";
 
-export default function Home({ data }: any) {
+export default function Home({}: any) {
+  const { data: session } = useSession();
+  const [expenses, setExpenses] = useState<any[]>([]);
+  const [user, setUser] = useState<any>();
+
+  useEffect(() => {
+    async function fetchData() {
+      if (session) {
+        const userData = await fetchUser(session?.user?.email as string);
+        const exps = await fetchExpenses(userData);
+        setExpenses(exps);
+        setUser(userData);
+      }
+    }
+
+    session && fetchData();
+  }, [session]);
+
   return (
     <div>
       <main>
-        <Dashboard
-          expenses={data.expenses ? JSON.parse(data.expenses) : []}
-          userData={data.userData}
-        />
+        <Dashboard expenses={expenses} userData={user} />
       </main>
     </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { res } = ctx;
-  let expenses = [];
-  let userData = null;
-
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
-
-  const session: Session | null = await getSession(ctx);
-
-  if (session) {
-    userData = await fetchUser(session?.user?.email as string);
-    expenses = await fetchExpenses(userData);
-  }
-
-  return {
-    props: {
-      data: {
-        expenses: expenses ? JSON.stringify(expenses) : [],
-        userData,
-      },
-    },
-  };
-};
